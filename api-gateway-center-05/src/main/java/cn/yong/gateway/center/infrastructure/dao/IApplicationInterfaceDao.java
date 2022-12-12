9be9b9f1eb8c3d1db4290c4f4ac819c894452e57
Allen <17317557314@163.com>
2022-12-12 22:21:13 +0800

    第13章：服务发现组件搭建和注册网关连接

--- api-gateway-center-05/src/main/java/cn/yong/gateway/center/infrastructure/dao/IApplicationInterfaceDao.java
+++ api-gateway-center-05/src/main/java/cn/yong/gateway/center/infrastructure/dao/IApplicationInterfaceDao.java
@@ -1,12 +1,14 @@
 package cn.yong.gateway.center.infrastructure.dao;
 
 import cn.yong.gateway.center.infrastructure.po.ApplicationInterface;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author Line
  * @desc 应用接口
  * @date 2022/12/4
  */
+@Mapper
 public interface IApplicationInterfaceDao {
 
     void insert(ApplicationInterface applicationInterface);
